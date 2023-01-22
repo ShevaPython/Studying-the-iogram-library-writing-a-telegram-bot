32788eaef66f6face451f74ecec66952b0317e05
Shevtsov Sergey <shevadotka@gmail.com>
2023-01-22 13:19:51 +0200

    lessons 1 echo bot and practic

---
+++ lesson_1/main.py
@@ -0,0 +1,27 @@
+
+import logging
+
+from aiogram import Bot, Dispatcher, executor, types
+
+import config
+from config import TOKEN
+
+
+API_TOKEN = TOKEN
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+
+    await message.reply(text=message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
---
+++ lesson_1/practice1.py
@@ -0,0 +1,27 @@
+from aiogram import Bot, types, executor, Dispatcher
+from config import TOKEN
+import logging
+
+# 1.Напишите бота который будет отправлять его же сообщения,переведенное в верхний регистер!
+# 2.Напишите бота который будет отправлять его же сообщения,если сообщения состоит более чем 2 слова!
+
+API_TOKEN = TOKEN
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=API_TOKEN)
+db = Dispatcher(bot)
+
+
+@db.message_handler()
+async def echo(message: types.Message):
+    if message.text.count(" ") == 0:
+        await message.answer(text=F"Эй ты чего {message.from_user.first_name} напиши больше чем 2 слова"
+                                 F"И я сделаю их в верхнем регистре!")
+
+    else:
+        await message.answer(text=F"{message.from_user.first_name} вот твое слово в верхнем регистре"
+                                  F" ->>   yes"
+                                  F"{message.text.upper()}")
+
+
+executor.start_polling(db, skip_updates=True)
