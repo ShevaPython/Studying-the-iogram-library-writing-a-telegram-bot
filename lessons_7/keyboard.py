592474f570447c25a6794adca9585bf3007fe91b
Shevtsov Sergey <shevadotka@gmail.com>
2023-01-31 14:58:56 +0200

    callback function

---
+++ lessons_7/keyboard.py
@@ -0,0 +1,16 @@
+# from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup
+#
+# kb = ReplyKeyboardMarkup(resize_keyboard=True)
+# button1 = KeyboardButton(text='/help')
+# button2 = KeyboardButton(text='/vote')
+# kb.add(button1, button2)
+#
+# ikb = InlineKeyboardMarkup(row_width=3)
+#
+# i_button1 = InlineKeyboardButton(text="❤️",
+#                                  callback_data='like')
+# i_button2 = InlineKeyboardButton(text="🙅‍♂️",
+#                                  callback_data='dislike')
+# i_button3 = InlineKeyboardButton(text="😎",
+#                                  callback_data='maybe')
+# ikb.add(i_button1,i_button2,i_button3)
